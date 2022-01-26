cf0d031cca9e736d3538848a0e9532ca17f11007
amjadkhansarwar <muhammad.amjad@iths.se>
2022-01-26 12:00:35 +0100

    last night work

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
+
 <template>
-<div id="app">
+ <!-- here i am calling my views -->
+<div id="app" class="app">
   <ActiveCard v-if="activetab === 1" :created="created" @sendtab="tabData"/>
   <AddCard v-else @send="addcard"/>
-  <AddData />
 </div>
 </template>
 
 <script>
-import AddCard from './component/AddCard'
-import AddData from './view/AddData'
-import ActiveCard from './component/ActiveCard'
-function sendDataLocalStorage(data){
+import AddCard from './view/AddCard'
+import ActiveCard from './view/ActiveCard'
+  function sendDataLocalStorage(data){
   localStorage.setItem("cardData", JSON.stringify(data))
-}
-function count(){
-      let data = localStorage.getItem("cardData")
-      if(data){
-        return data
-      }
-    }
+  }
+  function getData(){
+  let data = localStorage.getItem("cardData")
+  return data
+  }
 export default {
   components: {
     AddCard,
     ActiveCard,
-    AddData
     },
   data(){
     //localStorage.removeItem("cardData");
@@ -55,16 +52,9 @@ export default {
       return this.activetab
     },
     addcard(cardData){
-      console.log(cardData);
-      let validcard =cardData.cardnumber.toString().length
-      if(validcard < 16 || validcard > 16 ){
-        alert("You must put  only 16 digits for card number if you want to register it")
-        return
-      }
        let obj =[]
-       let counter =0
-       obj = count()
-       let i = 0
+        obj = getData()
+        if(obj){
        obj= JSON.parse(obj)
        let nycount = [...obj]
        for (const cardN of nycount) {
@@ -73,29 +63,32 @@ export default {
            return 
          }
        }
-        for ( i = 0; i < nycount.length; i++) {
-        if (nycount[i].vendor) 
-           counter++;
+          // let counter =0
+          // let i = 0
+          // for ( i = 0; i < nycount.length; i++) {
+          // if (nycount[i].vendor) 
+          // counter++;
+          // }
+          // if(counter >= 4)
+          // {
+          // alert("Your limt of cards in wallet is full now. you want to Uppdate  first one card and save this one")
+          // // alert(cardData.vendor)
+          // // alert(nycount)
+          // const result = nycount.filter(word => word.vendor != cardData.vendor);
+          // //alert(result)
+          // this.card = result
+          // // alert(this.card)
+          // sendDataLocalStorage(this.card)
+          // }
+       //alert(counter)
         }
-        if(counter >= 4)
-        {
-         alert("Your limt of cards in wallet is full now. you want to delete first one card and save this one")
-         alert(cardData.vendor)
-         alert(nycount)
-         const result = nycount.filter(word => word.vendor != cardData.vendor);
-         alert(result)
-         this.card = result
-          alert(this.card)
-          sendDataLocalStorage(this.card)
-        }
-       alert(counter)
       this.card.push({
         cardnumber: cardData.cardnumber,
         name: cardData.name,
         month: cardData.month,
         year: cardData.year,
         vendor:cardData.vendor
-      }),
+      })
       sendDataLocalStorage(this.card)
       this.activetab =1
     }
@@ -106,8 +99,8 @@ export default {
 
 <style >
 #app{
-  display: flex;
+  display: grid;
   justify-content: center;
-  align-items: center;
+  background-color: cadetblue;
 }
 </style>
